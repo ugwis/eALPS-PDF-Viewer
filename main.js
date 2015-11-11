3e71d36bcd45c23e0e07de0b6646d574218fab21
bgpat <bgpat@bgpat.net>
2015-11-12 00:20:03 +0900

    Reduce code size
    変更忘れ

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var obj = document.getElementById('resourceobject');
 if (obj) {
   var type = obj.getAttribute('type');
   if (type === 'application/pdf') {
-    location.replace(document.getElementById('resourceobject').getAttribute('data'));
+    location.replace(obj.getAttribute('data'));
   }
 }
